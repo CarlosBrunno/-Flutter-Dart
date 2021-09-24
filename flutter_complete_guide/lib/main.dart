7ec8a636da13a677ae2dc1c29700158271916628
CarlosBrunno <carlos.brunno97@gmail.com>
2021-09-24 14:26:23 -0300

    Button-to-function connection.

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestions() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -23,15 +27,15 @@ class MyApp extends StatelessWidget {
             Text('The question!'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestions,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestions,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: answerQuestions,
             ),
           ],
         ),
