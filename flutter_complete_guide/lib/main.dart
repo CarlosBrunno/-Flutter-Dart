a5a49f08c5c2cf96266e92c0ddff938c18d4d9d6
Carlos Brunno <carlos.brunno97@gmail.com>
2021-10-29 09:35:18 -0300

    New Custom Widget and First Styling & Layouting Steps

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './question.dart';
 //void main() {
 //  runApp(MyApp());
 //}
@@ -38,7 +38,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
+            Question(
               questions[_questionIndex],
             ),
             RaisedButton(
